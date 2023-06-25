01713ddb55117b8d8c6e32244bbc62cbf07536a9
Eyiniola Fagbemi <e.fagbemi@alustudent.com>
2023-06-25 02:56:41 -0700

    q1

---
+++ main.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+while true; do
+    echo "Welcome to the Bachelor of Software Engineering cohort list application!"
+    echo "Please select an option:"
+    echo "1. Create a student record"
+    echo "2. View all students"
+    echo "3. Delete a student record"
+    echo "4. Update a student record"
+    echo "5. Exit"
+
+    read -p "Enter your choice: " choice
