9a752316510a01cd6c946b68b6407e5c4c7ebff4
RainiBenaiah <b.raini@alustudent.com>
2023-06-26 02:51:04 -0700

    backup

--- backup-Negpod_ID.sh
+++ backup-Negpod_ID.sh
@@ -3,6 +3,6 @@
 host="a0dfc11ba139.a982e858.alu-cod.online"
 user="a0dfc11ba139"
 password="899c79fc532cd0591af1"
-directory="Group-repo/"
+directory="Group-repo"
 backup_location="/home/sftp-student/03033/summative"
 sshpass -p $password scp -r $directory $user@$host:$backup_location
