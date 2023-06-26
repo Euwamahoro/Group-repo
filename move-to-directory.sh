5794c298166ebc51416d180490d57d968c9e2f40
steven SHYAKA <shyastyve@gmail.com>
2023-06-26 12:03:01 -0700

    Steve on update

---
+++ move-to-directory.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# this is a script to move four files in question 1 to RW-SOFEN-8120120 directory
+mv main.sh Students-list_0333.txt Select-emails.sh student-emails.txt /RW-SOFEN-8120120
