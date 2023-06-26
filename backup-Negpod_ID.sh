36baa2f74497d323b22f08c6a0a4155195600704
RainiBenaiah <b.raini@alustudent.com>
2023-06-26 02:47:06 -0700

    backup

---
+++ backup-Negpod_ID.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Creates shell script to back up the directory in part a
+host="a0dfc11ba139.a982e858.alu-cod.online"
+user="a0dfc11ba139"
+password="899c79fc532cd0591af1"
+directory="Group-repo/"
+backup_location="/home/sftp-student/03033/summative"
+sshpass -p $password scp -r $directory $user@$host:$backup_location
