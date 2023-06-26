7209c3f99f12b882449258662f3c41491f5ad099
Euwamahoro <e.uwamahoro@alustudent.com>
2023-06-25 17:59:53 -0700

    This is Enock

---
+++ Summative-question_1/negpod_id-q1/negpod_id-q1/negpod_id-q1/move-to-directory.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Automatically move files to negpod_id-q1
+
+# Check if /Summative-question_1/ directory exists
+if [ -d "/Group-repo/Summative-question_1/" ]; then
+    LIST=$(ls /Group-repo/Summative-question_1/)
+
+    # Check if negpod_id-q1 directory does not exist
+    if [ ! -d "negpod_id-q1" ]; then
+        mkdir "negpod_id-q1"
+    fi
+
+    for FILE in $LIST; do
+        if [ -e "/Group-repo/Summative-question_1/$FILE" ]; then
+            cp -r "/Group-repo/Summative-question_1/$FILE" "negpod_id-q1"
+        fi
+done
+fi
