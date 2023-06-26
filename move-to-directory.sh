3af52c7463592296157215b2acdbeb706424a104
bmukankusi <b.mukankusi@alustudent.com>
2023-06-25 23:56:05 -0700

    The four files from q1 moved to a new directory

---
+++ move-to-directory.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#Create a directory called 0323-RW-SOFEN-8120120-9_q1
+
+mkdir 0323-RW-SOFEN-8120120-9_q1
+
+#Move all the four files from q1 in the new directory: 0323-RW-SOFEN-8120120-9_q1
+
+mv main.sh 0323-RW-SOFEN-8120120-9_q1
+mv select-emails.sh 0323-RW-SOFEN-8120120-9_q1
+mv student-emails.txt 0323-RW-SOFEN-8120120-9_q1
+mv students-list_0333.txt 0323-RW-SOFEN-8120120-9_q1
+
+echo "All four files successfully moved to the new directory"
